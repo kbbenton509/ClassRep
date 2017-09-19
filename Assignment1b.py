c53a0e78476937fadfc47381c86da69c5b1efc7e
kbbenton509 <kevinbenton46@yahoo.com>
2017-09-18 20:28:48 -0400

    Ver 2

--- Assignment1b.py
+++ Assignment1b.py
@@ -1,27 +1,21 @@
 import csv
 import unittest
 import glob, os
-path = os.getcwd()
+path = os.getcwd() #gets the current path of the directory, this is used later on to find if the xml file is in the directory
 os.chdir(path)
-os.remove("Test.xml")
+os.remove("Test.xml") #removes previous conversion xml file so the program will not give a false positive
 
-
-#xmlFile = 'Test.xml'
-#xData = open(xmlFile, 'w')
-#xData.write('<?xml version="1.0"?>' + "\n")
-#xData.write('<csv_data>' + "\n")
-
-def csvRead():
-	filename = raw_input('Enter the file you want to convert: ')
-	cData = csv.reader(filename)
-	convert(cData)
-def convert(Data):
-	xmlFile = 'Test.xml'
+def csvRead(): #function to read in a csv
+	filename = raw_input('Enter the file you want to convert: ') #user-input file name
+	cData = csv.reader(filename) #reads the file name csv to cData
+	convert(cData) #calls the function convert to convert the csv to xml
+def convert(Data): #function to convert a csv to xml
+	xmlFile = 'Test.xml' #opens a empty xml file so that we can write in it
 	xData = open(xmlFile, 'w')
 	xData.write('<?xml version="1.0"?>' + "\n")
 	xData.write('<csv_data>' + "\n")
 	rowNum = 0;
-	for row in Data:
+	for row in Data: #a for loop that converts the csv to xml row by row
 		if rowNum == 0:
 			tags = row
 			for i in range(len(tags)):
@@ -34,12 +28,10 @@ def convert(Data):
                 rowNum +=1
 	xData.write('</csv_data>' + "\n")
 	xData.close()
-def output():
-	for file in glob.glob("*.xml"):
-    		print('The conversion worked')
+def output(): #a function that outputs if there is a xml file in the directory 
+	for file in glob.glob("Test.xml"): #checks the directory for the xml file
+    		print('The conversion worked') #prints out a statement if the xml file was found
 
-#xData.write('</csv_data>' + "\n")
-#xData.close()
 
 if __name__ == "__main__":
 	csvRead();
--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import unittest
-import Assignment1b.py
+import Assignment1b.py #imports the Assignment file to use for the unittest
  
-class CTest(unittest.TestCase):
+class CTest(unittest.TestCase): #class used to test the functions
 	def test_f1(self):
-		self.failUnless(c.csvRead())
+		self.failUnless(c.csvRead()) #the test fails unless the function works properly
 	def test_f2(self):
-		self.assertEqual(c.output(), 'The conversion worked')
+		self.assertEqual(c.output(), 'The conversion worked') #tests to see if the function propely prints out the right statement
  
 if __name__ == '__main__':
 	unittest.main()
